d946df1a040373023e13fad92e7ee7d9745cad51
Lilaro <larochelle.lisa@gmail.com>
2020-02-04 22:30:58 -0500

    Finish FizzBuzz and start Fibonacci and Anagram

---
+++ Data Structures and Algorithms - Colt Steele, Udemy/Anagram.js/Anagram.js
@@ -0,0 +1,18 @@
+// Given two strings, write a function validAnagram to determine if the second string
+// is an anagram of the first.
+
+// validAnagram(cat, act) => true 
+// validAnagram(hello, hell) => false 
+
+// Edge Case Possibilities: spaces, numbers, punctuation, capitalized letters
+
+// Using frequency Counter pattern, create an object for each string that records
+// the count of each character/index
+
+// Pseudocode:
+// If the length of string1 is not equal to the length of string2, return false
+// Declare two empty objects, counter1 and counter2
+// For each character of string1, 
+
+
+
---
+++ Fibonacci.js
@@ -0,0 +1,15 @@
+// Take in an array of 2 numbers or just start by declaring an array of 2 numbers
+//  starting at 0
+// Take in a length?
+// Add 2 (index 0 & 1) numbers and push that sum onto the array
+// Add index 1 & 2 and push that sum onto the array etc...
+// Return the array
+
+
+
+function Fibonacci(len) {
+    let arr = [0, 1]
+    for (let i = 0; i <= len; i++) {
+        
+    }
+}
